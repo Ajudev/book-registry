f9b934ac068275cfda67020fed8cdca9dee4b862
ajudevnair <ajudevnair@gmail.com>
2022-07-31 16:00:44 +0400

    refactor: error handling for register api

--- routes/register.js
+++ routes/register.js
@@ -6,13 +6,16 @@ router.post("", async (req, resp) => {
   const hashedPassword = await bcrypt.hashSync(req.body.password);
   let query = "insert into Users values (?, ?, ?) ";
   dbQuery(query, [req.body.username, hashedPassword, req.body.email], (err) => {
-    if (err)
-      resp.status(400).send({
-        status: "Failed",
-        message: "User creation failed",
-        error: err.message,
-      });
-    else resp.status(200).send({ status: "Success", message: "User Created" });
+    err
+      ? resp.status(400).send({
+          status: "Failed",
+          message:
+            err.code === "ER_DUP_ENTRY"
+              ? "User already exists"
+              : "User creation failed",
+          error: err.message,
+        })
+      : resp.status(200).send({ status: "Success", message: "User Created" });
   });
 });
 
