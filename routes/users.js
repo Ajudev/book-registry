c96f8090517bbc1aa19ee9d611f173dfe8f6f570
ajudevnair <ajudevnair@gmail.com>
2022-08-07 16:28:05 +0400

    fix: Remove password from user data listing api's

--- routes/users.js
+++ routes/users.js
@@ -2,13 +2,24 @@ const router = require("express").Router();
 const dbQuery = require("../services/db");
 
 router.get("", async (req, resp) => {
-  const users = await dbQuery.user.findMany();
+  const users = await dbQuery.user.findMany({
+    select: {
+      id: true,
+      username: true,
+      email: true,
+    },
+  });
   resp.status(200).send({ status: "Success", data: users });
 });
 
 router.get("/:id", async (req, resp) => {
   const { id } = req.params;
   const user = await dbQuery.user.findUnique({
+    select: {
+      id: true,
+      username: true,
+      email: true,
+    },
     where: {
       username: id,
     },
