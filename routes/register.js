69dd9e384ea233d8e4db736b7ff73be4089f1fb6
Ajudev <ajudevnair@gmail.com>
2022-08-07 15:58:40 +0400

    feat: Integrate prisma orm instead of using MySQL package for database (#1)
    querying and migrations
    feat: Add middleware for error handling

--- routes/register.js
+++ routes/register.js
@@ -2,21 +2,25 @@ const router = require("express").Router();
 const bcrypt = require("bcryptjs");
 const dbQuery = require("../services/db");
 
-router.post("", async (req, resp) => {
-  const hashedPassword = await bcrypt.hashSync(req.body.password);
-  let query = "insert into Users values (?, ?, ?) ";
-  dbQuery(query, [req.body.username, hashedPassword, req.body.email], (err) => {
-    err
-      ? resp.status(400).send({
+router.post("", async (req, resp, next) => {
+  let hashedPassword = await bcrypt.hash(req.body.password, 10);
+  try {
+    const registerUser = await dbQuery.user.create({
+      data: {
+        username: req.body.username,
+        password: hashedPassword,
+        email: req.body.email,
+      },
+    });
+    "id" in registerUser
+      ? resp.status(200).send({ status: "Success", message: "User Created" })
+      : resp.status(400).send({
           status: "Failed",
-          message:
-            err.code === "ER_DUP_ENTRY"
-              ? "User already exists"
-              : "User creation failed",
-          error: err.message,
-        })
-      : resp.status(200).send({ status: "Success", message: "User Created" });
-  });
+          message: "User creation failed",
+        });
+  } catch (err) {
+    next(err);
+  }
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -2,35 +2,22 @@ const router = require("express").Router();
 const dbQuery = require("../services/db");
 
 router.get("", async (req, resp) => {
-  let query = "select username,email from Users";
-  dbQuery(query, (err, results) => {
-    console.log(results);
-    err
-      ? resp.status(400).send({
-          status: "Failed",
-          error: err.message,
-        })
-      : resp.status(200).send({ status: "Success", data: results });
-  });
+  const users = await dbQuery.user.findMany();
+  resp.status(200).send({ status: "Success", data: users });
 });
 
 router.get("/:id", async (req, resp) => {
   const { id } = req.params;
-  let query = "select username, email from Users where username=?";
-  dbQuery(query, [id], (err, results) => {
-    if (err)
-      resp.status(400).send({
-        status: "Failed",
-        error: err.message,
-      });
-    else {
-      !results.length
-        ? resp
-            .status(404)
-            .send({ status: "Failed", message: "No user with given id found" })
-        : resp.status(200).send({ status: "Success", data: results });
-    }
+  const user = await dbQuery.user.findUnique({
+    where: {
+      username: id,
+    },
   });
+  user
+    ? resp.status(200).send({ status: "Success", data: user })
+    : resp
+        .status(404)
+        .send({ status: "Failed", message: "No user with given id found" });
 });
 
 module.exports = router;
